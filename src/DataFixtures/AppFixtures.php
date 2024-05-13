57c8f7be430150d0d031612c90c4c094a82fa3e1
Souha2003 <souhahouiyette16@gmail.com>
2024-05-13 14:09:23 +0100

    Foundry: Fixtures You'll Love

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -9,23 +9,8 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        VinylMixFactory::createMany(25);
 
-
-        
-        $mix = new VinylMix();
-        $mix->setTitle('Do you Remember... Phil Collins?!');
-        $genres = ['pop', 'rock'];
-        $mix->setGenre($genres[array_rand($genres)]);
-        
-        $mix->setDescription('A pure mix of drummers turned singers!');
-        $mix->setGenre('pop');
-        $mix->setTrackCount(rand(5, 20));
-        $mix->setVotes(rand(-50, 50));
-
-        $manager->persist($mix);
-        
         $manager->flush();
 
     }
